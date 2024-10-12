5578d6b869e5906560a0c4e0165f8f668ae19357
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-12 09:06:55 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -917,6 +917,9 @@ class EMA(nn.Module):
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
         self.gn = nn.GroupNorm(channels // self.groups, channels // self.groups)
         self.conv1x1 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=1, stride=1, padding=0)
+        self.conv2_h = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=1, stride=1, padding=0)
+        self.conv2_w = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=1, stride=1, padding=0)
+        
         self.conv3x3 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
@@ -926,7 +929,7 @@ class EMA(nn.Module):
         x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
         hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
         x_h, x_w = torch.split(hw, [h, w], dim=2)
-        x1 = self.gn(group_x * x_h.sigmoid() * x_w.permute(0, 1, 3, 2).sigmoid())
+        x1 = self.gn(group_x * self.conv2_h(x_h.sigmoid()) *self.conv2_w(x_w.permute(0, 1, 3, 2)).sigmoid())
         x2 = self.conv3x3(group_x)
         x11 = self.softmax(self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
         x12 = x2.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
@@ -934,4 +937,3 @@ class EMA(nn.Module):
         x22 = x1.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
         weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
         return (group_x * weights.sigmoid()).reshape(b, c, h, w)
-
