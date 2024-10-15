4c137cb4cbddd4ca504c422a7206ccdb7a50ec48
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-15 10:51:28 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -926,13 +926,13 @@ class EMA(nn.Module):
         self.conv1x1 = nn.Conv2d(channels , channels // self.groups, kernel_size=1, stride=1, padding=0)
         self.conv2_h = nn.Conv2d(channels // self.groups, channels , kernel_size=1, stride=1, padding=0)
         self.conv2_w = nn.Conv2d(channels // self.groups, channels , kernel_size=1, stride=1, padding=0)      
-        self.conv_final = nn.Conv2d(2*channels, channels, kernel_size=3, padding=1)  # Extra convolution to combine features
+        self.conv3x3 = nn.Conv2d(channels, channels // self.groups, kernel_size=3, padding=1)  # Extra convolution to combine features
 
         
         self.pool_h = nn.AdaptiveAvgPool2d((None, 1))  # Keep height dimension
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))  # Keep width dimension
         
-        self.activation = nn.ReLU()  # ReLU activation
+     #   self.activation = nn.ReLU()  # ReLU activation
 
     def forward(self, x):
         b, c, h, w = x.size()
@@ -950,14 +950,15 @@ class EMA(nn.Module):
         # Processing for x2 (vertical features)
         x_h_inv = self.pool_w(x).permute(0, 1, 3, 2)  # Inverted pooled height
         x_w_inv = self.pool_h(x)  # Inverted pooled width
-        hw_inv = self.conv1x1(torch.cat([x_h_inv, x_w_inv], dim=2))  # Concatenate with flipped dimensions
+        hw_inv = self.conv3x3(torch.cat([x_h_inv, x_w_inv], dim=2))  # Concatenate with flipped dimensions
         x_h_inv, x_w_inv = torch.split(hw_inv, [w, h], dim=2)  # Split back into height and width
 
         # Calculate features for x2 using the inverse operations
         x2_processed = x * self.conv2_h(x_h_inv.sigmoid()) * self.conv2_w(x_w_inv.permute(0, 1, 3, 2)).sigmoid()
-        combined=torch.cat([x1, x2_processed], dim=1)
+        output=x1+x2_processed
+      {  combined=torch.cat([x1, x2_processed], dim=1)
     # Perform matrix multiplication (element-wise)
-        output = self.activation(self.conv_final(combined))  # Pass through conv3x3
+    #    output = self.activation(self.conv_final(combined))  # Pass through conv3x3
 
     # Combine height and width features using an additional convolution
 
