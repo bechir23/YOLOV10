6df13fc5182b855c0103c55930895fde68582c99
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-17 23:53:31 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -4,6 +4,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad ,  SpatialAttention, ChannelAttention
 from .transformer import TransformerBlock
