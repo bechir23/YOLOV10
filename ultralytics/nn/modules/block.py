420df17f6a474fd930a3fa60a034897fec658e73
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-18 20:01:03 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -39,7 +39,8 @@ __all__ = (
     "CBFuse",
     "CBLinear",
     "Silence",
-    "ResNetBlock"
+    "ResNetBlock",
+    "DeformableAttention"
 )
 
 
@@ -845,6 +846,169 @@ class C2fCIB(C2f):
         x = (v @ attn.transpose(-2, -1)).view(B, C, H, W) + self.pe(v.reshape(B, C, H, W))
         x = self.proj(x)
         return x"""
+from torch import nn, einsum
+from einops import rearrange
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def divisible_by(numer, denom):
+    return (numer % denom) == 0
+
+def create_grid_like(t, dim=0):
+    h, w, device = *t.shape[-2:], t.device
+    grid = torch.stack(torch.meshgrid(
+        torch.arange(w, device=device),
+        torch.arange(h, device=device),
+    indexing='xy'), dim=dim)
+    grid.requires_grad = False
+    grid = grid.type_as(t)
+    return grid
+
+def normalize_grid(grid, dim=1, out_dim=-1):
+    h, w = grid.shape[-2:]
+    grid_h, grid_w = grid.unbind(dim=dim)
+    grid_h = 2.0 * grid_h / max(h - 1, 1) - 1.0
+    grid_w = 2.0 * grid_w / max(w - 1, 1) - 1.0
+    return torch.stack((grid_h, grid_w), dim=out_dim)
+
+class Scale(nn.Module):
+    def __init__(self, scale):
+        super().__init__()
+        self.scale = scale
+
+    def forward(self, x):
+        return x * self.scale
+
+class DeformableAttention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head=64,
+        heads=8,
+        dropout=0.,
+        downsample_factor=4,
+        offset_scale=None,
+        offset_groups=None,
+        offset_kernel_size=6,
+        group_queries=True,
+        group_key_values=True,
+        depth=2,
+    ):
+        super().__init__()
+        offset_scale = default(offset_scale, downsample_factor)
+        assert offset_kernel_size >= downsample_factor, 'offset kernel size must be greater than or equal to the downsample factor'
+        assert divisible_by(offset_kernel_size - downsample_factor, 2)
+
+        offset_groups = default(offset_groups, heads)
+        assert divisible_by(heads, offset_groups)
+
+        inner_dim = dim_head * heads
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        self.offset_groups = offset_groups
+
+        offset_dims = inner_dim // offset_groups
+
+        self.downsample_factor = downsample_factor
+
+        self.to_offsets = nn.Sequential(
+            nn.Conv2d(offset_dims, offset_dims, offset_kernel_size, groups=offset_dims, stride=downsample_factor, padding=(offset_kernel_size - downsample_factor) // 2),
+            nn.GELU(),
+            nn.Conv2d(offset_dims, 2, 1, bias=False),
+            nn.Tanh(),
+            Scale(offset_scale)
+        )
+
+        self.mlp = nn.ModuleList([
+            nn.Sequential(nn.Linear(2, dim), nn.ReLU())
+        ])
+        for _ in range(depth - 1):
+            self.mlp.append(nn.Sequential(nn.Linear(dim, dim), nn.ReLU()))
+        self.mlp.append(nn.Linear(dim, heads // offset_groups))
+
+        self.dropout = nn.Dropout(dropout)
+        self.to_q = nn.Conv2d(dim, inner_dim, 1, groups=offset_groups if group_queries else 1, bias=False)
+        self.to_k = nn.Conv2d(dim, inner_dim, 1, groups=offset_groups if group_key_values else 1, bias=False)
+        self.to_v = nn.Conv2d(dim, inner_dim, 1, groups=offset_groups if group_key_values else 1, bias=False)
+        self.to_out = nn.Conv2d(inner_dim, dim, 1)
+
+    def forward(self, x, return_vgrid=False):
+        heads, b, h, w, downsample_factor, device = self.heads, x.shape[0], *x.shape[-2:], self.downsample_factor, x.device
+
+        # Queries
+        q = self.to_q(x)
+
+        # Calculate offsets
+        group = lambda t: rearrange(t, 'b (g d) ... -> (b g) d ...', g=self.offset_groups)
+        grouped_queries = group(q)
+        offsets = self.to_offsets(grouped_queries)
+
+        # Calculate grid + offsets
+        grid = create_grid_like(offsets)
+        vgrid = grid + offsets
+        vgrid_scaled = normalize_grid(vgrid)
+
+        # Sample from the input using grid sampling
+        kv_feats = F.grid_sample(
+            group(x),
+            vgrid_scaled,
+            mode='bilinear', padding_mode='zeros', align_corners=False
+        )
+        kv_feats = rearrange(kv_feats, '(b g) d ... -> b (g d) ...', b=b)
+
+        # Derive key / values
+        k, v = self.to_k(kv_feats), self.to_v(kv_feats)
+
+        # Scale queries
+        q = q * self.scale
+
+        # Split out heads
+        q, k, v = map(lambda t: rearrange(t, 'b (h d) ... -> b h (...) d', h=heads), (q, k, v))
+
+        # Query / key similarity
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        # Relative positional bias
+        grid = create_grid_like(x)
+        grid_scaled = normalize_grid(grid, dim=0)
+        pos_bias = self.compute_rel_pos_bias(grid_scaled, vgrid_scaled)
+        sim = sim + pos_bias
+
+        # Numerical stability
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+
+        # Attention
+        attn = sim.softmax(dim=-1)
+        attn = self.dropout(attn)
+
+        # Aggregate and combine heads
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x=h, y=w)
+        out = self.to_out(out)
+
+        if return_vgrid:
+            return out, vgrid
+
+        return out
+
+    def compute_rel_pos_bias(self, grid_q, grid_kv):
+        device, dtype = grid_q.device, grid_kv.dtype
+
+        grid_q = rearrange(grid_q, 'h w c -> 1 (h w) c')
+        grid_kv = rearrange(grid_kv, 'b h w c -> b (h w) c')
+
+        pos = rearrange(grid_q, 'b i c -> b i 1 c') - rearrange(grid_kv, 'b j c -> b 1 j c')
+        bias = torch.sign(pos) * torch.log(pos.abs() + 1)  # log of distance is sign(rel_pos) * log(abs(rel_pos) + 1)
+
+        for layer in self.mlp:
+            bias = layer(bias)
+
+        bias = rearrange(bias, '(b g) i j o -> b (g o) i j', g=self.offset_groups)
+        return bias
 
 class Attention(nn.Module):
     def __init__(self, dim, num_heads=8, attn_ratio=0.5, image_size=(32, 32)):
