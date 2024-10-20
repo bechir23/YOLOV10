252bf1bd439a623a171fdda4ff074b6f4916ec57
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-20 20:10:25 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -1117,25 +1117,33 @@ class CoordAtt(nn.Module):
         # Pooling along height and width
         x_h = self.pool_h(x)  # Pool over height
         x_w = self.pool_w(x)  # Pool over height
-
+        z_h=x_h.permute(0,1,3,2)
+        z_w=x_w
+     
         x_w = x_w.permute(0, 1, 3, 2)  # Transpose width and height
-
+    
         # Concatenation of x_h and x_w
         y = torch.cat([x_h, x_w], dim=2)
+        z = torch.cat([z_h, z_w], dim=3)
 
         # First convolution
         y = self.conv1(y)
         y = self.coord_act(y)
+        z = self.conv1(z)
+        z = self.coord_act(z)
+
 
         # Split into x_h and x_w
         x_h, x_w = torch.split(y, [h, w], dim=2)
+        z_h, z_w = torch.split(z, [h, w], dim=3)
 
         # Transpose x_w back to original shape
         x_w = x_w.permute(0, 1, 3, 2)
+        z_h = x_z.permute(0, 1, 3, 2)
 
         # Second and third convolutions with sigmoid activations
-        a_h = torch.sigmoid(self.conv2(x_h))
-        a_w = torch.sigmoid(self.conv3(x_w))
+        a_h = torch.sigmoid(self.conv2(x_h))+ torch.sigmoid(self.conv2(z_h))
+        a_w = torch.sigmoid(self.conv3(x_w)) + torch.sigmoid(self.conv2(z_w))
 
         # Output
         out = x * a_h * a_w
