8980f8fcd607691918cca496eb8ad8a2f1e27b7a
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-10-25 13:46:13 +0100

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -153,27 +153,27 @@ class Focus(nn.Module):
         # return self.conv(self.contract(x))
 """
 import torch.nn.functional as F
-
 class Focus(nn.Module):
-    """Focus layer to reduce the spatial dimensions and enhance feature extraction."""
-    
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1,):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels * 4, out_channels, kernel_size, stride, padding)
+    """Focus layer to reduce spatial dimensions and enhance feature extraction."""
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, act=True):
+        super(Focus, self).__init__()
+        self.conv = nn.Conv2d(in_channels * 4, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.act = nn.SiLU()  # Activation function
+        self.act = nn.SiLU() if act else nn.Identity()
 
     def forward(self, x):
-
-        # Slice the input tensor and concatenate along the channel dimension
         x = torch.cat([
-            x[..., ::2, ::2],   # Top left
-            x[..., 1::2, ::2],  # Bottom left
-            x[..., ::2, 1::2],  # Top right
-            x[..., 1::2, 1::2]  # Bottom right
+            x[..., ::2, ::2],  # Top-left
+            x[..., 1::2, ::2],  # Bottom-left
+            x[..., ::2, 1::2],  # Top-right
+            x[..., 1::2, 1::2]  # Bottom-right
         ], dim=1)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        return x
 
-        return  self.act(self.bn(self.conv(x)))
 class GhostConv(nn.Module):
     """Ghost Convolution https://github.com/huawei-noah/ghostnet."""
 
