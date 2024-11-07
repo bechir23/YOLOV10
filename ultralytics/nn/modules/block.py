3792ad82dda308aaa2e042e3e5fa554ddfa815dd
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-07 18:22:44 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -226,29 +226,6 @@ class C2f(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(BottleneckCSP(self.c, self.c,1,shortcut,g, e=1.0) for _ in range(n))
-
-    def forward(self, x):
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend(m(y[-1]) for m in self.m)
-        output=self.cv2(torch.cat(y, 1))
-        output.tag = "c2f"
-        return output
-
-    def forward_split(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in self.m)
-
-        return self.cv2(torch.cat(y, 1))
-"""class C2f(nn.Module):
-
-    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
-        
-        super().__init__()
-       
-        self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
@@ -260,7 +237,7 @@ class C2f(nn.Module):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
  
-        return self.cv2(torch.cat(y, 1))"""
+        return self.cv2(torch.cat(y, 1))
 
 
 class C3(nn.Module):
