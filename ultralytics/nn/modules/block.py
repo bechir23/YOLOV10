fe9274320cdd9bd11d25d637a3030c3276b838dc
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-07 16:12:58 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -217,14 +217,34 @@ class C2(nn.Module):
         a, b = self.cv1(x).chunk(2, 1)
         return self.cv2(torch.cat((self.m(a), b), 1))
 
-
 class C2f(nn.Module):
-    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
-        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
-        expansion.
-        """
+        
+        super().__init__()
+       
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(BottleneckCSP(self.c, self.c,1,shortcut,g, e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        print("c2f x", x.shape)
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        output=self.cv2(torch.cat(y, 1))
+        output.tag = "c2f"
+        return output
+
+    def forward_split(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+
+        return self.cv2(torch.cat(y, 1))
+"""class C2f(nn.Module):
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        
         super().__init__()
        
         self.c = int(c2 * e)  # hidden channels
@@ -233,20 +253,15 @@ class C2f(nn.Module):
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
-        """Forward pass through C2f layer."""
-       # print("c2f x", x.shape)
         y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
-      #  print("c2f y 1",(self.cv2(torch.cat(y, 1))).shape)
         return self.cv2(torch.cat(y, 1))
 
     def forward_split(self, x):
-        """Forward pass using split() instead of chunk()."""
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
  
-        #print("c2f y 2",(self.cv2(torch.cat(y, 1))).shape)
-        return self.cv2(torch.cat(y, 1))
+        return self.cv2(torch.cat(y, 1))"""
 
 
 class C3(nn.Module):
