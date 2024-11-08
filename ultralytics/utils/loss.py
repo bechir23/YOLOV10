9e9135848e213500fdb4267468a690b251d79bec
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-08 14:03:04 +0100

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -231,7 +231,7 @@ class v8DetectionLoss:
         target_scores_sum = max(target_scores.sum(), 1)
 
         # Cls loss
-        loss[1] = self.varifocal_loss(pred_scores, target_scores, target_labels) / target_scores_sum  # VFL way
+        loss[1] = self.varifocal_loss(pred_scores, target_scores, gt_labels) / target_scores_sum  # VFL way
         #loss[1] = self.bce(pred_scores, target_scores.to(dtype)).sum() / target_scores_sum  # BCE
 
         # Bbox loss
