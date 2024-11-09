9ec83ea55f08fbcdf2c158373f795aeb02c47142
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-09 19:31:09 +0100

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -158,11 +158,11 @@ class v8DetectionLoss:
         self.nc = m.nc  # number of classes
         self.no = m.no
         self.reg_max = m.reg_max
-    #    self.focal_loss = FocalLoss() 
+        self.focal_loss = FocalLoss() 
         self.device = device
 
         self.use_dfl = m.reg_max > 1
-        self.varifocal_loss=VarifocalLoss()
+  #      self.varifocal_loss=VarifocalLoss()
         self.assigner = TaskAlignedAssigner(topk=tal_topk, num_classes=self.nc, alpha=0.5, beta=6.0)
         self.bbox_loss = BboxLoss(m.reg_max - 1, use_dfl=self.use_dfl).to(device)
         self.proj = torch.arange(m.reg_max, dtype=torch.float, device=device)
