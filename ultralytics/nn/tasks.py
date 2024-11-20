b15b2ca9f8c2b6b36a480ea4c06f1789f84c14ea
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-20 18:40:39 +0100

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -61,7 +61,8 @@ from ultralytics.nn.modules import (
     CoordAtt,
     EMA,
     DeformableAttention,
-    LCAN
+    LCAN,
+    C3k2,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
