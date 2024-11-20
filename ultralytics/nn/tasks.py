ab0411163c3c3919ebe393c3520442f7c7bd84c2
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-20 17:14:55 +0100

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -61,6 +61,7 @@ from ultralytics.nn.modules import (
     CoordAtt,
     EMA,
     DeformableAttention,
+    LCAN
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -943,6 +944,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             c2 = ch[f[-1]]
         elif m is CBAM :
             c1=ch[f]
+            args=[c1]
+        elif m is LCAN :
+            ch1=ch[f]
             args=[c1]
         elif m is ECAAttention:
             c1, c2 = ch[f], args[0]
