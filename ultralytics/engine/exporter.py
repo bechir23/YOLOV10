fa08c58ebd24bed9b32d870bc8def740409c24fc
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-11-23 10:09:57 +0100

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -78,9 +78,11 @@ from ultralytics.nn.tasks import DetectionModel, SegmentationModel, WorldModel
 from ultralytics.utils import (
     ARM64,
     DEFAULT_CFG,
+    IS_JETSON,
     LINUX,
     LOGGER,
     MACOS,
+    PYTHON_VERSION,
     ROOT,
     WINDOWS,
     __version__,
