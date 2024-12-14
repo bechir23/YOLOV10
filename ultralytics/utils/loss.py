fe949aa3ed0d0720ebcdecd6c939958b444b5cb7
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-12-14 22:48:56 +0100

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -188,7 +188,8 @@ class v8DetectionLoss:
         """Decode predicted object bounding box coordinates from anchor points and distribution."""
         if self.use_dfl:
             b, a, c = pred_dist.shape  # batch, anchors, channels
-            pred_dist = self.proj.to(self.device)
+            device = pred_dist.device
+            pred_dist = self.proj.to(device)
             pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(self.proj.type(pred_dist.dtype))
             # pred_dist = pred_dist.view(b, a, c // 4, 4).transpose(2,3).softmax(3).matmul(self.proj.type(pred_dist.dtype))
             # pred_dist = (pred_dist.view(b, a, c // 4, 4).softmax(2) * self.proj.type(pred_dist.dtype).view(1, 1, -1, 1)).sum(2)
