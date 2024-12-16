98b40790462fefc87b82d15e59c65ec5a0db3229
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-12-16 17:47:09 +0100

    Update validator.py

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -152,7 +152,7 @@ class BaseValidator:
             self.dataloader = self.dataloader or self.get_dataloader(self.data.get(self.args.split), self.args.batch)
 
             model.eval()
-            model.warmup(imgsz=( self.args.batch, 3, imgsz, imgsz))  # warmup
+     #       model.warmup(imgsz=( self.args.batch, 3, imgsz, imgsz))  # warmup
 
         self.run_callbacks("on_val_start")
         dt = (
