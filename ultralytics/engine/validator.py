3082b171840bb0c9e9fdce83665f3d69b7aec5d4
BECHIR BEN TEKFA <130161716+bechir23@users.noreply.github.com>
2024-12-16 17:30:41 +0100

    Update validator.py

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -143,12 +143,7 @@ class BaseValidator:
            # stride, pt, jit, engine = model.stride, model.pt, model.jit, model.engine
             
             imgsz = check_imgsz(self.args.imgsz, stride=64)
-            if engine:
-                self.args.batch = model.batch_size
-            elif not pt and not jit:
-                self.args.batch = 1  # export.py models default to batch-size 1
-                LOGGER.info(f"Forcing batch=1 square inference (1,3,{imgsz},{imgsz}) for non-PyTorch models")
-
+            
             if str(self.args.data).split(".")[-1] in ("yaml", "yml"):
                 self.data = check_det_dataset(self.args.data)
             elif self.args.task == "classify":
